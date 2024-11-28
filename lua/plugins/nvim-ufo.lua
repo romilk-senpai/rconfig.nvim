5c8af59d063f2b3a5ce458d6de100ff215984c38
romilk-senpai <rpakhomov1702@gmail.com>
2024-11-28 04:21:57 +0200

    folidng plug kawaii

---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,23 @@
+return {
+  'kevinhwang91/nvim-ufo',
+  dependencies = { 'kevinhwang91/promise-async' },
+  config = function()
+    vim.o.foldcolumn = '0'
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    local language_servers = require('lspconfig').util.available_servers()
+    for _, ls in ipairs(language_servers) do
+      require('lspconfig')[ls].setup {
+        capabilities = capabilities,
+      }
+    end
+    require('ufo').setup()
+  end,
+}
